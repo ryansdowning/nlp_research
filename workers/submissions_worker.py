febcf87a087520fff16a48391c55b54ae80ccf02
ryansdowning <ryansdowning1@gmail.com>
2020-08-15 23:29:55 -0400

    feat(workers): Added workers to run from command line to collect data

---
+++ workers/submissions_worker.py
@@ -0,0 +1,28 @@
+import argparse
+from rsutils import data_utils as du
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    '-s',
+    '--subreddit',
+    default='investing',
+    type=str,
+    help="subreddit to get comments from",
+)
+parser.add_argument(
+    '-f',
+    '--file',
+    default='comments_stream.csv',
+    type=str,
+    help="path of comments output file"
+)
+parser.add_argument(
+    '--fields',
+    default=None,
+    nargs='*',
+    type=str
+)
+
+args = parser.parse_args()
+du.stream_subreddit_submissions(args.subreddit, args.file, args.fields)
