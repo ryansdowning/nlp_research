4c25c61c0f6112757665f4887b3bef23f8aa368a
ryansdowning <ryansdowning1@gmail.com>
2020-11-20 01:17:32 -0500

    feat(scrape_utils): Added SQL table insertion functionality to streams and abstracted away streaming functionality to work with any generator type of stream

--- scrape_utils/data_utils.py
+++ scrape_utils/data_utils.py
@@ -1,9 +1,14 @@
-from loguru import logger
-import os
 import csv
+import os
+from typing import Any, Generator, Iterable, List, Optional, Tuple
+
+from loguru import logger
+
+from nlp_utils import db_utils as dbu
+from nlp_utils import tagging_utils as tu
 
 
-def open_file_stream(file, data_fields):
+def open_file_stream(file: str, data_fields: List[str]):
     """Prepares file for streaming data into
 
     Args:
@@ -29,3 +34,112 @@ def open_file_stream(file, data_fields):
             csv_out = csv.writer(out)
             csv_out.writerow(data_fields)
     return 0
+
+
+def process_attr(obj, field):
+    """Helper function to extract attributes and nested attributes from objects
+
+    Args:
+        obj: object that contains [field] as attribute
+        field: attribute of object
+
+    Returns:
+        Attribute of object corresponding to the respective field
+    """
+    out = obj
+    for attr in field.split('.'):
+        out = getattr(out, attr)
+    return out
+
+
+def get_attributes_list(obj: Any, data_fields: Iterable[str]) -> Tuple[Any]:
+    """Gets the values of the given attributes from the object
+
+    Args:
+        obj: Object to get values from - must have all attributes in data fields parameter
+        data_fields: The attributes to extract from the object
+
+    Returns:
+        List of values extracted from the object, respective to the given data fields (in order)
+    """
+    return tuple(map(lambda field: process_attr(obj, field), data_fields))
+
+
+def get_data_fields(data: Iterable[Any], fields: Iterable[str]):
+    """Gets a list of attributes for [n] of objects of the same type
+
+    Args:
+        data: Iterable of objects containing [fields]
+        fields: attributes to extract from each data object
+
+    Returns:
+        List of tuples containing the [fields] from each object in data
+        Size will be len(data) x len(fields)
+    """
+    return [get_attributes_list(point, fields) for point in data]
+
+
+def stream_data(
+        stream: Generator[Any, None, None],
+        data_fields: List[str] = None,
+        file: Optional[str] = None,
+        table: Optional[dbu.DBTable] = None,
+        keywords: Optional[List[str]] = None,
+        keyword_fields: Optional[str] = None,
+        case_sensitive: bool = None,
+        **kwargs
+):
+    """Creates a process that indefinitely streams data from a subreddit to a file
+
+    Args:
+        stream: Generator that outputs a stream of data corresponding to the given data fields
+        data_fields: Ordered list of attributes to extract from submissions/comments
+        keywords: If None, this argument has no affect. If keywords are provided, comments are submissions are filtered
+                  if they do not contain a keyword in their body or selftext respectively
+        keyword_fields: String corresponding to the data field(s) to match keywords for. If multiple are provided, the
+                       fields will be joined by a space before searching for keywords.
+        case_sensitive: If keywords are provided, flag for case sensitive matching. Default True, on.
+        file: Name of file (csv) to write data to
+        table: DBTable object that can be provided to insert data directly into SQL database
+
+    Returns:
+        None - Process runs until error is thrown or is interrupted
+        Data stream is written to the given file
+    """
+    if not file and not table:
+        raise AttributeError("Must provide at least one of <file> or <table> parameters to stream data to!")
+    if keywords is not None and keyword_fields is None:
+        raise AttributeError("Keywords were provided without specifying any keyword fields to match to!")
+
+    if keywords:
+        if isinstance(keyword_fields, str):
+            keyword_fields = {keyword_fields}
+
+        keyword_fields = set(keyword_fields)
+        try:
+            idxs = [data_fields.index(field) for field in keyword_fields]
+        except ValueError:
+            raise AttributeError(
+                f"Could not find all keyword fields in the provided data fields!\n"
+                f"Missing: {keyword_fields - set(data_fields)}"
+            )
+        proc = tu.keyword_bool_proc(keywords=keywords, case_sensitive=case_sensitive)
+    else:
+        idxs = None
+        proc = None
+
+    if file:
+        open_file_stream(file, data_fields)
+    cols = ', '.join(data_fields)
+    for data in stream:
+        if proc is not None:  # If filtering for keywords, run keyword check on keyword fields
+            keyword_data = ' '.join(data[idx] for idx in idxs)
+            if not proc(keyword_data):  # If no match, skip this iteration
+                continue
+        if table:
+            table.insert(data, data_fields)
+        if file:
+            with open(file, 'a', encoding='utf-8') as out:
+                logger.info('writing %s data to to file at: %s' % (cols, file))
+                csv_out = csv.writer(out)
+                csv_out.writerow(data)
--- scrape_utils/reddit_utils.py
+++ scrape_utils/reddit_utils.py
@@ -1,13 +1,13 @@
-import csv
 from typing import Any, Iterable, Iterator, List, Optional, Tuple, Union
 
 import pandas as pd
 import praw
 from loguru import logger
 
+from config.constants import (COMMENT_FIELDS, SUBMISSION_FIELDS,
+                              SUBMISSION_SORTS, reddit)
+from nlp_utils import db_utils as dbu
 from scrape_utils import data_utils as du
-from config.constants import (
-    COMMENT_FIELDS, COMMENT_SORTS, SUBMISSION_FIELDS, SUBMISSION_SORTS, reddit)
 
 
 def get_submissions(subreddit: praw.reddit.Subreddit, sort_method: str, **kwargs) -> Iterator[Any]:
@@ -71,23 +71,6 @@ def filter_marked(
     return [entity for entity in data if getattr(entity, field) not in marked_labels]
 
 
-def _get_attributes_list(obj: Any, data_fields: Iterable[str]) -> Tuple[Any]:
-    """Gets the values of the given attributes from the object
-
-    Args:
-        obj: Object to get values from - must have all attributes in data fields parameter
-        data_fields: The attributes to extract from the object
-
-    Returns:
-        List of values extracted from the object, respective to the given data fields (in order)
-    """
-    return tuple(map(lambda field: getattr(obj, field), data_fields))
-
-
-def _get_data_fields(data: Iterable[Any], fields: Iterable[str]):
-    return [_get_attributes_list(point, fields) for point in data]
-
-
 def get_submissions_data(
         submissions: Iterable[praw.reddit.Submission], data_fields: Iterable[str]
 ) -> List[Tuple]:
@@ -106,7 +89,7 @@ def get_submissions_data(
             f" {set(data_fields) - set(SUBMISSION_FIELDS)}"
         )
 
-    data = _get_data_fields(submissions, data_fields)
+    data = du.get_data_fields(submissions, data_fields)
     return data
 
 
@@ -128,7 +111,7 @@ def get_comments_data(
             f" {set(data_fields) - set(COMMENT_FIELDS)}"
         )
 
-    data = _get_data_fields(comments, data_fields)
+    data = du.get_data_fields(comments, data_fields)
     return data
 
 
@@ -237,28 +220,25 @@ def get_subreddit_submission_comments(
     return data
 
 
-def _stream_subreddit_data(
+def subreddit_data_stream(
         subreddit: str,
         sub_or_comm: str,
-        file: str,
-        data_fields: List[str],
-        **kwargs
+        data_fields: List[str] = None,
+        **kwargs,
 ):
-    """Creates a process that indefinitely streams data from a subreddit to a file
+    """Creates a generator expression that yields a stream of data from a subreddit given the input parameters
 
     Args:
         subreddit: Name of subreddit
         sub_or_comm: identify type of data to stream: 'submissions' or 'comments'
-        file: Name of file (csv) to write data to
         data_fields: Ordered list of attributes to extract from submissions/comments
         kwargs: Additional keyword arguments provided to the prawcore stream function for submissions/comments
 
     Returns:
-        None - Process runs until error is thrown or is interrupted
-        Data stream is written to the given file
+        Tuples of len(data_fields) are yielded respective to the ordered data fields
     """
     if sub_or_comm not in ('submissions', 'comments'):
-        raise AttributeError("Must specify either submissions or comments for streaming data")
+        raise AttributeError("Must specify either submissions or comments for data stream")
     sub = reddit.subreddit(subreddit)
     data_stream = (
         sub.stream.submissions(**kwargs)
@@ -266,24 +246,24 @@ def _stream_subreddit_data(
         else sub.stream.comments(**kwargs)
     )
 
-    du.open_file_stream(file, data_fields)
     for data in data_stream:
-        extracted = _get_attributes_list(data, data_fields)
-        with open(file, 'a', encoding='utf-8') as out:
-            logger.info('writing %s data to to file at: %s' % (sub_or_comm, file))
-            csv_out = csv.writer(out)
-            csv_out.writerow(extracted)
+        yield du.get_attributes_list(data, data_fields)
 
 
 def stream_subreddit_submissions(
-        subreddit: str, file: str, data_fields: Optional[List[str]] = None, **kwargs
+        subreddit: str,
+        data_fields: Optional[List[str]] = None,
+        file: Optional[str] = None,
+        table: Optional[dbu.DBTable] = None,
+        **kwargs
 ):
     """Creates a process that indefinitely streams submissions data from a subreddit to a file
 
     Args:
         subreddit: Name of subreddit
-        file:  Name of file (csv) to write data to
         data_fields: Ordered list of attributes to extract from submissions
+        file:  Name of file (csv) to write data to
+        table: DBTable object that can be provided to insert data directly into SQL database
         kwargs: Additional keyword arguments provided to the prawcore submissions stream function
 
     Returns:
@@ -298,18 +278,24 @@ def stream_subreddit_submissions(
             f" {set(data_fields) - set(SUBMISSION_FIELDS)}"
         )
 
-    _stream_subreddit_data(subreddit, 'submissions', file, data_fields, **kwargs)
+    stream = subreddit_data_stream(subreddit, 'submissions', data_fields, **kwargs)
+    du.stream_data(stream=stream, data_fields=data_fields, file=file, table=table)
 
 
 def stream_subreddit_comments(
-        subreddit: str, file: str, data_fields: Optional[List[str]] = None, **kwargs
+        subreddit: str,
+        data_fields: Optional[List[str]] = None,
+        file: Optional[str] = None,
+        table: Optional[dbu.DBTable] = None,
+        **kwargs
 ):
     """Creates a process that indefinitely streams comments data from a subreddit to a file
 
     Args:
         subreddit: Name of subreddit
-        file:  Name of file (csv) to write data to
         data_fields: Ordered list of attributes to extract from comments
+        file:  Name of file (csv) to write data to
+        table: DBTable object that can be provided to insert data directly into SQL database
         kwargs: Additional keyword arguments provided to the prawcore comments stream function
 
     Returns:
@@ -324,7 +310,8 @@ def stream_subreddit_comments(
             f" {set(data_fields) - set(COMMENT_FIELDS)}"
         )
 
-    _stream_subreddit_data(subreddit, 'comments', file, data_fields, **kwargs)
+    stream = subreddit_data_stream(subreddit, 'comments', data_fields, **kwargs)
+    du.stream_data(stream, data_fields, file, table)
 
 
 def _update_data(
@@ -373,12 +360,12 @@ def _update_data(
     if sub_or_comm == 'submissions':
 
         def get_updated(id_):
-            return _get_attributes_list(reddit.submission(id_), data.columns)
+            return du.get_attributes_list(reddit.submission(id_), data.columns)
 
     else:
 
         def get_updated(id_):
-            return _get_attributes_list(reddit.comment(id_), data.columns)
+            return du.get_attributes_list(reddit.comment(id_), data.columns)
 
     data = pd.DataFrame(data[id_col].apply(get_updated).values.tolist(), columns=data.columns)
     if file is not None:
