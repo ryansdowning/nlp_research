6afab5fe6a37b7318c5962e657352b0b5e06a1c2
Ryan Downing <ryansdowning1@gmail.com>
2021-01-17 13:50:46 -0500

    fix(config/constants): Added request headers

--- config/constants.py
+++ config/constants.py
@@ -1,9 +1,9 @@
 import praw
 
-client_id = None
-secret = None
-agent = None
-reddit = praw.Reddit(client_id=client_id, client_secret=secret, user_agent=agent)
+reddit_client_id = None
+reddit_secret = None
+reddit_agent = None
+reddit = praw.Reddit(client_id=reddit_client_id, client_secret=reddit_secret, user_agent=reddit_agent)
 
 SUBMISSION_SORTS = ('hot', 'new', 'rising', 'controversial', 'top', 'gilded')
 COMMENT_SORTS = ('best', 'top', 'new', 'controversial', 'old', 'qa')
@@ -18,3 +18,8 @@ COMMENT_FIELDS = (
     'link_id', 'parent_id', 'permalink', 'replies', 'score', 'stickied', 'submission', 'subreddit',
     'subreddit_id'
 )
+
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko)'
+                  ' Chrome/58.0.3029.110 Safari/537.36',
+}
