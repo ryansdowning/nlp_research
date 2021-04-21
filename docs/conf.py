50733cf29f43eca6c010506361b155ea758d8fd3
Ryan Downing <ryansdowning1@gmail.com>
2021-04-20 21:48:35 -0400

    chore(ci): Fix docs and dependencies

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'stock_sentiment'
+project = 'finlang'
 copyright = '2021, Ryan Downing'
 author = 'Ryan Downing'
 
